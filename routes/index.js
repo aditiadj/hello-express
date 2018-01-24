d998594df20471f69427f6f8b602b0b0a82348c9
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-01-24 11:44:40 +0700

    expressjs with a simple api

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+router.get('/api', (req, res) => {
+
+  res.send("Hello Express!!")
+});
+
+module.exports = router;
